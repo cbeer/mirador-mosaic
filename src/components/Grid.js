2b4925b88b54396b466cc461a2d7fa31fd2d5d64
Chris Beer <chris@cbeer.info>
2023-02-15 14:47:37 -0800

    Use inset

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -30,10 +30,7 @@ function Container({
 
   const gridStyle = {
     position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
+    inset: 0,
     display: 'grid',
     gap: '16px',
     justifyItems: 'stretch',
